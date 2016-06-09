6fa4d96c158f5b3799cd798967c82e7e783900de
Igor Nascimento <hieifn@gmail.com>
2016-06-09 16:15:43 -0300

    Trabalho semi pronto: Senha so aceita letras minusculas. Colocar um lower case na senha e decidir se so serao aceitas letras.

--- Pythonmelhor.pl
+++ Pythonmelhor.pl
@@ -1,5 +1,4 @@
 use warnings;
-
 use strict;
 
 
@@ -8,22 +7,53 @@ sub code{
     my @charsMensagem = split("", $mensagem);
     my @charsSenha = split("", $senha);
     my $contador=0;
-
     foreach (@charsMensagem){
-
+        my $i='a';
         #Expandir depois esse regex para incuir outros caracteres
         if (/[a-zA-Z]/){
-            for (my $i='A',my $i==@charsSenha[$contador % $tamanho],my $i++){
+            while($i ne @charsSenha[$contador % $tamanho]){
                 ++$_;
+                ++$i;
             }
-        $contador++;
+            my @letra = split("",$_);
+            $_= $letra[-1];
+            $contador++;
+            #print $_,"\n";
         }
     }
-
     return (@charsMensagem);
 }
 
-my $c= "abacaxi";
-my $v= "senha";
-my $lenght= 5;
-print code($c,$v, $lenght), "\n";
+sub decode{
+    my ($mensagem, $senha, $tamanho) = @_;
+    my @charsMensagem = split("", $mensagem);
+    my @charsSenha = split("", $senha);
+    my $contador=0;
+    foreach (@charsMensagem){
+        my $i='a';
+        #Expandir depois esse regex para incuir outros caracteres
+        if (/[a-zA-Z]/){
+            while($i ne @charsSenha[$contador % $tamanho]){
+
+                for (my $j=0; $j<25; $j++){#HACK PARA FAZER DECREMENTO, JA QUE PERL NÃO CONSEGUE DECREMENTAR
+                    ++$_;
+                }
+                ++$i;
+            }
+            my @letra = split("",$_);
+            $_= $letra[-1];
+            $contador++;
+            #print $_,"\n";
+        }
+    }
+    return (@charsMensagem);
+}
+#my @alfabeto=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+#sfnjapm
+
+my $m= "Eduardo";
+my $s= "asa";
+my $lenght= 3;
+#print code($m,$s, $lenght), "\n";
+print decode($m,$s, $lenght), "\n";
+
