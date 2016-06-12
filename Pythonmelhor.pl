3931bb9d45ec42d4488282bb2b7a3b6efe93c7a8
Igor Nascimento <hieifn@gmail.com>
2016-06-12 20:19:29 -0300

    adicionei os numeros na senha ! nada demais ! so to sem saco de projetar antena para aquele cara extremamente respeitoso, no qual eu quero muito bem ( != ) . eh nois nasla ! Depois da um confere ai.

--- Pythonmelhor.pl
+++ Pythonmelhor.pl
@@ -75,7 +75,7 @@ sub decode{
 #my @alfabeto=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 #sfnjapm
 ##Eduardo asa
-my $m= "2222";
+my $m= "22222";
 my $s= "1";
 my $lenght= 1;
 #print code($m,$s, $lenght), "\n";
